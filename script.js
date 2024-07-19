b175dad5df580481892f8d52ab2d275331f291a9
jawid fakhari <jawid_fakhari@live.it>
2024-07-19 11:45:30 +0200

    line from spline created, tube from spline line created

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 import * as THREE from "three";
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import spline from "./spline";
+import spline from './spline.js';
 
 const w = window.innerWidth;
 const h = window.innerHeight;
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
+camera.position.z = 5;
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(w, h);
 document.body.appendChild(renderer.domElement);
@@ -14,12 +15,26 @@ const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 controls.dampingFactor = 0.03;
 
+// create a line from spline
+const points = spline.getPoints(100);
+const geometry = new THREE.BufferGeometry().setFromPoints(points);
+const material = new THREE.LineBasicMaterial({ color: 0xff0000});
+const line = new THREE.Line(geometry, material);
+scene.add(line);
+
+// create tube from spline
+const tube = new THREE.TubeGeometry(spline, 222, 0.65, 16, true);
+const tubeMaterial = new THREE.MeshStandardMaterial({ color: 0xfffff, wireframe: true });
+const tubeMesh = new THREE.Mesh(tube, tubeMaterial);
+scene.add(tubeMesh);
+
+
 const hemiLight = new THREE.HemisphereLight(0xffffff, 0x000000, 1);
 scene.add(hemiLight);
 
 
 function animate() {
-    mesh1.rotation.y += 0.01;
+    // mesh1.rotation.y += 0.01;
     renderer.render(scene, camera);
 }
 renderer.setAnimationLoop(animate);
