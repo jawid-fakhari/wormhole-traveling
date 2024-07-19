8b196f77f84377d5b52dcbc6f15e50a30398be6b
jawid fakhari <jawid_fakhari@live.it>
2024-07-19 12:00:38 +0200

    function update camera fly thru the tube added

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 import * as THREE from "three";
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import spline from './spline.js';
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import spline from "./spline.js";
 
 const w = window.innerWidth;
 const h = window.innerHeight;
@@ -16,25 +16,37 @@ controls.enableDamping = true;
 controls.dampingFactor = 0.03;
 
 // create a line from spline
-const points = spline.getPoints(100);
-const geometry = new THREE.BufferGeometry().setFromPoints(points);
-const material = new THREE.LineBasicMaterial({ color: 0xff0000});
-const line = new THREE.Line(geometry, material);
-scene.add(line);
+// const points = spline.getPoints(100);
+// const geometry = new THREE.BufferGeometry().setFromPoints(points);
+// const material = new THREE.LineBasicMaterial({ color: 0xff0000 });
+// const line = new THREE.Line(geometry, material);
+// scene.add(line);
 
 // create tube from spline
 const tube = new THREE.TubeGeometry(spline, 222, 0.65, 16, true);
-const tubeMaterial = new THREE.MeshStandardMaterial({ color: 0xfffff, wireframe: true });
+const tubeMaterial = new THREE.MeshStandardMaterial({
+  color: 0xfffff,
+  wireframe: true,
+});
 const tubeMesh = new THREE.Mesh(tube, tubeMaterial);
 scene.add(tubeMesh);
-
 
 const hemiLight = new THREE.HemisphereLight(0xffffff, 0x000000, 1);
 scene.add(hemiLight);
 
+function updateCamera(t) {
+  const time = t * 0.2;
+  const looptime = 20 * 1000;
+  const p = (time % looptime) / looptime;
+  const pos = tube.parameters.path.getPointAt(p);
+  const lookAt = tube.parameters.path.getPointAt((p + 0.03) % 1);
+  camera.position.copy(pos);
+  camera.lookAt(lookAt);
+}
 
-function animate() {
-    // mesh1.rotation.y += 0.01;
-    renderer.render(scene, camera);
+function animate(t = 0) {
+  updateCamera(t);
+  // mesh1.rotation.y += 0.01;
+  renderer.render(scene, camera);
 }
 renderer.setAnimationLoop(animate);
