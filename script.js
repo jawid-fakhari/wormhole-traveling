3198368c6ba1f4a5cd037ded3a14e6ee8514840a
jawid fakhari <jawid_fakhari@live.it>
2024-07-24 13:11:08 +0200

    box geometry added inside the tube geometry

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ import spline from "./spline.js";
 const w = window.innerWidth;
 const h = window.innerHeight;
 const scene = new THREE.Scene();
+//add fog to scene
+scene.fog = new THREE.FogExp2(0x000000, 0.3);
 const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
 camera.position.z = 5;
 const renderer = new THREE.WebGLRenderer();
@@ -16,20 +18,46 @@ controls.enableDamping = true;
 controls.dampingFactor = 0.03;
 
 // create a line from spline
-// const points = spline.getPoints(100);
-// const geometry = new THREE.BufferGeometry().setFromPoints(points);
-// const material = new THREE.LineBasicMaterial({ color: 0xff0000 });
-// const line = new THREE.Line(geometry, material);
-// scene.add(line);
+const points = spline.getPoints(100);
+const geometry = new THREE.BufferGeometry().setFromPoints(points);
+const material = new THREE.LineBasicMaterial({ color: 0xff0000 });
+const line = new THREE.Line(geometry, material);
+scene.add(line);
 
 // create tube from spline
-const tube = new THREE.TubeGeometry(spline, 222, 0.65, 16, true);
+const tubeGeo = new THREE.TubeGeometry(spline, 222, 0.65, 16, true);
 const tubeMaterial = new THREE.MeshStandardMaterial({
   color: 0xfffff,
   wireframe: true,
 });
-const tubeMesh = new THREE.Mesh(tube, tubeMaterial);
+const tubeMesh = new THREE.Mesh(tubeGeo, tubeMaterial);
 scene.add(tubeMesh);
+
+// create box geometry and add it to the scene inside the tube geometry
+const boxNumber = 55;
+const size = 0.2;
+const boxGeometry = new THREE.BoxGeometry(size, size, size);
+const boxMaterial = new THREE.MeshBasicMaterial({
+  color: 0xffffff,
+  wireframe: true,
+});
+let box;
+for (let i = 0; i <boxNumber; i++) {
+  box = new THREE.Mesh(boxGeometry, boxMaterial);
+  const p = (i / boxNumber + Math.random() * 0.1) % 1;
+  const pos = tubeGeo.parameters.path.getPointAt(p);
+  pos.x += Math.random() - 0.4;
+  pos.z += Math.random() - 0.4;
+  box.position.copy(pos);
+  const rote = new THREE.Vector3(
+    Math.random() - 0.4,
+    Math.random() - 0.4,
+    Math.random() - 0.4
+  );
+  box.rotation.set(rote.x, rote.y, rote.z);
+  
+  scene.add(box);
+}
 
 const hemiLight = new THREE.HemisphereLight(0xffffff, 0x000000, 1);
 scene.add(hemiLight);
@@ -38,14 +66,15 @@ function updateCamera(t) {
   const time = t * 0.2;
   const looptime = 20 * 1000;
   const p = (time % looptime) / looptime;
-  const pos = tube.parameters.path.getPointAt(p);
-  const lookAt = tube.parameters.path.getPointAt((p + 0.03) % 1);
+  const pos = tubeGeo.parameters.path.getPointAt(p);
+  const lookAt = tubeGeo.parameters.path.getPointAt((p + 0.03) % 1);
   camera.position.copy(pos);
   camera.lookAt(lookAt);
 }
 
 function animate(t = 0) {
   updateCamera(t);
+  box.rotation.y += 0.01;
   // mesh1.rotation.y += 0.01;
   renderer.render(scene, camera);
 }
