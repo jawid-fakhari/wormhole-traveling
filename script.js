f43b18f6e6ad957101642a4614493a15b9121c54
jawid fakhari <jawid_fakhari@live.it>
2024-07-25 10:24:38 +0200

    bloom effect added to the scene

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import spline from "./spline.js";
+import { EffectComposer } from "three/addons/postprocessing/EffectComposer.js";
+import { RenderPass } from "three/addons/postprocessing/RenderPass.js";
+import { UnrealBloomPass } from "three/addons/postprocessing/UnrealBloomPass.js";
 
 const w = window.innerWidth;
 const h = window.innerHeight;
@@ -16,6 +19,18 @@ document.body.appendChild(renderer.domElement);
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 controls.dampingFactor = 0.03;
+
+// create a reneder pass for the camera and scene
+const renderScene = new RenderPass(scene, camera);
+const composer = new EffectComposer(renderer);
+composer.addPass(renderScene);
+
+// create a bloom effect
+const bloomPass = new UnrealBloomPass(new THREE.Vector2(w, h), 5, 1, 0);
+composer.addPass(bloomPass);
+
+// tone mapping 
+
 
 // create a line from spline
 // const points = spline.getPoints(100);
@@ -42,7 +57,7 @@ const boxMaterial = new THREE.MeshBasicMaterial({
   wireframe: true,
 });
 let box;
-for (let i = 0; i <boxNumber; i++) {
+for (let i = 0; i < boxNumber; i++) {
   box = new THREE.Mesh(boxGeometry, boxMaterial);
   // randomize box position
   const p = (i / boxNumber + Math.random() * 0.1) % 1;
@@ -51,7 +66,7 @@ for (let i = 0; i <boxNumber; i++) {
   pos.x += Math.random() - 0.5;
   pos.z += Math.random() - 0.5;
   box.position.copy(pos);
-  
+
   // randomize box rotation
   const rote = new THREE.Vector3(
     Math.random() - Math.PI,
@@ -59,13 +74,13 @@ for (let i = 0; i <boxNumber; i++) {
     Math.random() - Math.PI
   );
   box.rotation.set(rote.x, rote.y, rote.z);
-  
+
   const edges = new THREE.EdgesGeometry(boxGeometry, 0.2);
-  const lineMat = new THREE.LineBasicMaterial({color: 0xffff00});
+  const lineMat = new THREE.LineBasicMaterial({ color: 0xffff00 });
   const boxLines = new THREE.LineSegments(edges, lineMat);
   boxLines.position.copy(pos);
   boxLines.rotation.copy(rote);
-  
+
   // scene.add(box);
   scene.add(boxLines);
 }
@@ -86,6 +101,9 @@ function updateCamera(t) {
 function animate(t = 0) {
   updateCamera(t);
   // mesh1.rotation.y += 0.01;
-  renderer.render(scene, camera);
+  // renderer.render(scene, camera);
+  composer.render();
+  requestAnimationFrame(animate);
 }
-renderer.setAnimationLoop(animate);
+// renderer.setAnimationLoop(animate);
+animate();
